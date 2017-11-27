284fe3f186c537541aa5d5d064d4ce3c96165d4b
claudiotazza <claudiotazza@gmail.com>
2017-11-27 13:57:43 +0100

    fatto cose belle

--- es3.cpp
+++ es3.cpp
@@ -16,7 +16,8 @@ void stampa_lista(lista* p){
     //stampa il valore del nodo
     stampa_lista((*p).next);
     //stampa il resto della lista con una chiamata ricorsiva
-  } 
+  }
+ 
 }
 
 lista* crea_lista_input(lista * p){ 
@@ -38,7 +39,7 @@ lista * rimuovi_n_lista(lista* p, int n){
   if(p == NULL) return NULL;
   
   else{ 
-    if( ((*p).x % n) == 0){
+    if( ( ((*p).x % n) == 0 ) && ((*p).x != n)){
       lista * tmp;
       tmp = p;
       p = (*p).next;
@@ -50,23 +51,33 @@ lista * rimuovi_n_lista(lista* p, int n){
       (*p).next = rimuovi_n_lista((*p).next, n);
       return head;
     }//Se il val va bene il nodo non viene toccato 
-     //e next e' uguale al resto della lista senza i nodi non vouti
+     //e next e' uguale al resto della lista senza i nodi non voluti
   }    
 }
 
-int main(){
-  int  n;
+lista * crivello_di_era(lista * p){
+  lista * head = p;
+  if(p == NULL) return NULL;
 
+  else{
+    if((*p).x >=2){
+      p = rimuovi_n_lista(p, (*p).x);//rimuovi_n_lista modificata
+      p = crivello_di_era((*p).next);
+      return head;
+    }
+    else p = crivello_di_era((*p).next);
+
+  }
+}
+
+int main(){
   lista* p;
   //la nostra lista  
 
   cout << "inserire i valori per la lista (0 per concludere)." << endl;
   p = crea_lista_input(p);
 
-  cout << "Inserisci il numero da rimuovere" << endl;
-  cin >> n;
-
-  p = rimuovi_n_lista(p, n);
+  p = crivello_di_era(p);
 
   cout << "\nLista:" << endl;
   stampa_lista(p);
